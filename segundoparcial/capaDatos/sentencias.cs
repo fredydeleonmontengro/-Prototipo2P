150be196e08bcaa4f088586d5e8214f6cfcbf14a
Fredy-de-leon <fedel21m@gmail.com>
2020-04-29 09:50:47 -0600

    contenedor

---
+++ segundoparcial/capaDatos/sentencias.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaDatos
+{
+    public class sentencias
+    {
+        conexion cn = new conexion();
+        OdbcCommand comm;
+
+    }
+}
---
+++ segundoparcial/capaLogica/logica.cs
@@ -0,0 +1,15 @@
+﻿using capaDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaLogica
+{
+    public class logica
+    {
+        sentencias sn = new sentencias();
+
+    }
+}
